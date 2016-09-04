41535e99330e96106f08439790654d2381e65b82
M.Seo <meongheeseo@gmail.com>
2016-09-04 13:04:02 +0900

    20160904 ADAM4024 added

--- VISA_test/MainWindow.xaml.cs
+++ VISA_test/MainWindow.xaml.cs
@@ -29,6 +29,7 @@ namespace VISA_test
         private ViStatus viStatus;
         private Thread thread;
         private SerialPort serialport;
+        private SerialPort adam4024;
 
         public MainWindow()
         {
@@ -71,10 +72,19 @@ namespace VISA_test
 
         private void stop_btn_Click(object sender, RoutedEventArgs e)
         {
+            stopAll();
+        }
+
+        private void stopAll()
+        {
             start = false;
             thread.Abort();
             thread.Join();
+
+            adam4024.Write("#00C0-05.000\r\n");
+
             serialport.Close();
+            adam4024.Close();
             msgbox.AppendText("*****     Stop Data Read     *****\n");
         }
 
@@ -82,6 +92,10 @@ namespace VISA_test
         private void readData()
         {
             msgbox.AppendText("*****     Start Data Read     *****\n");
+
+            int step = 100;
+            int i = 0;
+            int max = -1;
 
             while (start)
             {
@@ -109,10 +123,21 @@ namespace VISA_test
                 else
                 {
                     msgbox.AppendText(retMsg.ToString(0, 13));
-                    serialport.Write(retMsg.ToString(0, 13) + "   ");
+                    double visa_data = Convert.ToDouble(retMsg.ToString(0, 13));
+                    double data = i * ((max + 5) / step) - 5;
+                    String msg = "#00C0" + formatNum(data) + "\r\n";
+
+                    adam4024.Write(msg);
+                    //serialport.Write(retMsg.ToString(0, 13) + "   ");
                 }
 
                 Thread.Sleep(1000);
+                i++;
+
+                if (step == i)
+                {
+                    stopAll();
+                }
             }
         }
 
@@ -128,6 +153,65 @@ namespace VISA_test
                 MessageBox.Show("Unable to conenct to serial port", "Warning", MessageBoxButton.OK, MessageBoxImage.Error);
             }
 
+            String adamSerialPort = "";
+            // ADAM 4024 Connection
+            try
+            {
+                adam4024 = new SerialPort(adamSerialPort, 9600);
+                adam4024.Open();
+                adam4024.DataReceived += new SerialDataReceivedEventHandler(DataReceivedHandler);
+
+                adam4024.Write("%0000000600\r\n");
+                msgbox.AppendText("%0000000600\r\n");
+                wait(0.5);
+                
+                adam4024.Write("$007C0R32\r\n");
+                msgbox.AppendText("$007C0R32\r\n");
+                wait(0.5);
+                
+                adam4024.Write("#00SC0-05.000\r\n");
+                msgbox.AppendText("#00SC0-05.000\r\n");
+                wait(0.5);
+                
+                adam4024.Write("$004\r\n");
+                msgbox.AppendText("$004\r\n");
+                wait(0.5);
+            }
+            catch (Exception e)
+            {
+                MessageBox.Show("Unable to connect to ADAM", "Warning", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+         
+        private void DataReceivedHandler(object sender, SerialDataReceivedEventArgs e)
+        {
+            SerialPort sp = (SerialPort)sender;
+            msgbox.AppendText(sp.ReadExisting() + "\r\n");
+        }
+
+        public static void wait(double seconds)
+        {
+            var frame = new DispatcherFrame();
+
+            new Thread((ThreadStart)(() =>
+                {
+                    Thread.Sleep(TimeSpan.FromSeconds(seconds));
+                    frame.Continue = false;
+                })).Start();
+
+            Dispatcher.PushFrame(frame);
+        }
+
+        private static String formatNum(double a)
+        {
+            if (a >= 0)
+            {
+                return (String.Format("{0, 0:+00.000}\r\n", a));
+            }
+            else
+            {
+                return (String.Format("{0, 0:00.000}\r\n", a));
+            }
         }
     }
 }
